30a468a58572b527144f79e879b7ddf6bcea2657
Mark Tranter <mark.a.tranter@gmail.com>
2017-06-07 08:37:32 +0100

    Add css

--- ui/src/skills/admin/edit.js
+++ ui/src/skills/admin/edit.js
@@ -7,7 +7,7 @@ export class Edit {
     this.svc = skillsSvc;
     this.opts = {max:100, step:5}
   }
-  attached() {
+  attached() {chrome
     this.skillsSvc.getSkills().then(s => this.skills = s);
   }
   updateSkill(skill) {
